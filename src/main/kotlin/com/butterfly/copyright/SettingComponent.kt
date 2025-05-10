4a45a5a7af60cea2396e9459ca933c0d9c6be820
dingqiang-miliyoga <dingqiang@miliyoga.com>
2025-05-10 18:05:28 +0800

    保存设置

--- src/main/kotlin/com/butterfly/copyright/SettingComponent.kt
+++ src/main/kotlin/com/butterfly/copyright/SettingComponent.kt
@@ -1,15 +1,13 @@
 package com.butterfly.copyright
 
 import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.components.JBTextField
 import com.intellij.ui.dsl.builder.*
-import javax.swing.Box
-import javax.swing.BoxLayout
-import javax.swing.JCheckBox
 import javax.swing.JComponent
-import javax.swing.JPanel
 
 class SettingComponent : Configurable {
-    private var panel: Panel? = null
+    private var panel: DialogPanel? = null
     private var setting1: String = ""
     private var setting2: Boolean = false
     private var setting3: Int = 0
@@ -23,17 +21,25 @@ class SettingComponent : Configurable {
         setting2 = settings.isFeatureEnabled
         setting3 = settings.numberOption
 
-        return panel {
+        panel = panel {
             group("Basic Settings") {
                 row("Configuration Value:") {
                     textField()
                         .bindText(::setting1)
+                        .onChanged { textField ->
+                            setting1 = textField.text
+                            println("setting2: $setting1")
+                        }
                         .comment("Enter your configuration value here")
                 }
 
                 row {
                     checkBox("Enable")
                         .bindSelected(::setting2)
+                        .onChanged { checkBox ->
+                            setting2 = checkBox.isSelected
+                            println("setting2: $setting2")
+                        }
                         .gap(RightGap.SMALL)
 
                     contextHelp("When enabled, activates advanced functionality")
@@ -42,9 +48,14 @@ class SettingComponent : Configurable {
                 row("Number Option:") {
                     intTextField(1..100)
                         .bindIntText(::setting3)
+                        .onChanged { field ->
+                            setting3 = field.text.toIntOrNull()!!
+                            println("setting3: $setting3")
+                        }
                 }
             }
         }
+        return panel!!
     }
 
     override fun isModified(): Boolean {
